385e18590fde7a75ebbd4a6c6abdd3e0d114d3ad
mparati31 <mparati31@gmail.com>
2022-03-25 13:24:17 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# albert-vscode
+
+Simple unofficial [albert-launcher](https://github.com/albertlauncher/albert) extension that shows and opens recent Visual Studio Code files and folders.
+
+The trigger to activate the extension is `vs`.
+
+## Installation
+
+To install the extension type the following in your terminal:
+
+```
+git clone https://github.com/mparati31/albert-vscode ~/.local/share/albert/org.albert.extension.python/modules/vscode
+```
+
+Now active the extension: open albert's settings and go to `Extensions > Python > Visual Studio Code` and check the checkbox.
---
+++ __init__.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+
+"""Open Visual Studio Code recents file and folders.
+
+Synopsis: <trigger> <filter>"""
+
+import os
+import json
+
+from albert import *
+from pathlib import Path
+from shutil import which
+
+__title__ = 'Visual Studio Code'
+__version__ = '0.4.3'
+__triggers__ = ['vs']
+__authors__ = 'mparati'
+
+iconPath = os.path.dirname(__file__) + '/icon.png'
+vscodeRecentsPath = Path.home() / '.config' / 'Code' / 'storage.json'
+
+
+def getVscodeRecents():
+    try:
+        storage = json.load(open(vscodeRecentsPath, 'r'))
+        items = storage['lastKnownMenubarData']['menus']['File']['items']
+    except:
+        return [], []
+
+    for item in items:
+        if item['id'] == 'submenuitem.34':
+            items_filtred = item
+
+    recents = items_filtred['submenu']['items']
+
+    folders = []
+    files = []
+    for recent in recents:
+        if recent['id'] == 'openRecentFolder':
+            folders.append(recent['uri']['path'])
+        elif recent['id'] == 'openRecentFile':
+            files.append(recent['uri']['path'])
+
+    return folders, files
+
+
+def resizePath(path, maxchars=50):
+    if len(path) <= maxchars:
+        return path
+
+    parts = path.split('/')
+    parts.reverse()
+
+    relativeLen = 0
+    shortPath = ''
+
+    for part in parts:
+        if len(part) == 0: continue
+        if len('.../' + part + '/' + shortPath) <= maxchars:
+            shortPath = '/' + part + shortPath
+            relativeLen += len(part)
+        else:
+            break
+
+    return '...' + shortPath
+
+
+def startWithSpace(string):
+    return len(string) > 0 and not string.isspace() and string[0] != ' '
+
+
+def makeRecentItem(path, recentType):
+    return makeItem(
+        resizePath(path),
+        f'Open Recent: {recentType}',
+        [TermAction('', f'code "{path}"')]
+    )
+
+
+def makeItem(text, subtext='', actions=[]):
+    return Item(
+                id = __title__,
+                icon = iconPath,
+                text = text,
+                subtext = subtext,
+                actions = actions
+            )
+
+
+def handleQuery(query):
+    if not query.isTriggered:
+        return None
+
+    if not which('code'):
+        return makeItem('Visual Studio Code not installed')
+
+    string = query.string
+
+    if startWithSpace(string):
+        return None
+
+    string = string.strip().lower()
+    folders, files = getVscodeRecents()
+
+    if not folders and not files:
+        return makeItem('Recents Files and Folders not found')
+
+    items = []
+
+    for el in folders + files:
+        if not string in el.lower():
+            continue
+
+        items.append(makeRecentItem(el, 'Folder' if el in folders else 'File'))
+
+    return items
